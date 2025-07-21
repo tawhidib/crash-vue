dd169df595d8a8d385ce8ca4433b392d223fb5a0
tawhidib <tawhidib@gmail.com>
2025-07-21 06:13:25 +0600

    vue composition api example added

---
+++ src/AppComposition.vue
@@ -0,0 +1,87 @@
+<script setup>
+import { ref } from "vue";
+
+const name = "Md Towhidul Islam";
+const status = ref("active");
+const tasks = ["task 1", "task 2", "task 3"];
+const githubLink = "https://github.com/tawhidib";
+
+const toggleStatus = () => {
+    switch (status.value) {
+        case "active":
+            status.value = "pending";
+            break;
+
+        case "pending":
+            status.value = "inactive";
+            break;
+
+        case "inactive":
+            status.value = "active";
+            break;
+
+        default:
+            status.value = "active";
+            break;
+    }
+};
+
+// initial version of setup api start from here [script setup is no need]
+
+// export default {
+//     setup() {
+//         const name = "Md Towhidul Islam";
+//         const status = ref("active");
+//         const tasks = ["task 1", "task 2", "task 3"];
+//         const githubLink = "https://github.com/tawhidib";
+
+//         const toggleStatus = () => {
+//             switch (status.value) {
+//                 case "active":
+//                     status.value = "pending";
+//                     break;
+
+//                 case "pending":
+//                     status.value = "inactive";
+//                     break;
+
+//                 case "inactive":
+//                     status.value = "active";
+//                     break;
+
+//                 default:
+//                     status.value = "active";
+//                     break;
+//             }
+//         };
+
+//         return {
+//             name,
+//             status,
+//             tasks,
+//             githubLink,
+//             toggleStatus,
+//         };
+//     },
+// };
+</script>
+
+<template>
+    <h1>Hello, 👋 !!! My name is {{ name }}</h1>
+
+    <!-- status related data start from here  -->
+    <p v-if="status === 'active'">My status is 'Active' now</p>
+    <p v-else-if="status === 'pending'">My status is 'Pending' now</p>
+    <p v-else>My status is 'Inactive' now</p>
+
+    <button @click="toggleStatus">Play with Status</button>
+    <!-- status related data ends here  -->
+
+    <!-- tasks start from here  -->
+    <ul>
+        <li v-for="task in tasks" :key="task">{{ task }}</li>
+    </ul>
+    <!-- tasks start ends here  -->
+
+    <p>If you are interested for see my github profile, <a :href="githubLink" target="_blank">Click here</a></p>
+</template>
