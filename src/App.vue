f9ee8caf7bc2fd22d656eb05007a5c88d5b293a6
tawhidib <tawhidib@gmail.com>
2025-07-22 06:58:43 +0600

    replace all the component in the component folder

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
-import HeroSection from "./HeroSection.vue";
-import HomeCards from "./HomeCards.vue";
-import Navbar from "./Navbar.vue";
+import HeroSection from "./components/HeroSection.vue";
+import HomeCards from "./components/HomeCards.vue";
+import JobListing from "./components/JobListing.vue";
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
     <Navbar />
     <HeroSection title="This is the title" subtitle="this is the subtitle" />
     <HomeCards />
+    <JobListing />
 </template>
---
+++ src/components/JobListing.vue
@@ -0,0 +1,111 @@
+<script setup>
+import jobData from "@/jobs.json";
+import { ref } from "vue";
+
+const jobs = ref(jobData);
+
+console.log({ data: jobs.value });
+</script>
+
+<template>
+    <section class="bg-green-50 px-4 py-10">
+        <div class="container-xl lg:container m-auto">
+            <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">Browse Jobs</h2>
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <!-- Job Listing 1 -->
+                <div class="bg-white rounded-xl shadow-md relative">
+                    <div class="p-4">
+                        <div class="mb-6">
+                            <div class="text-gray-600 my-2">Full-Time</div>
+                            <h3 class="text-xl font-bold">Senior Vue Developer</h3>
+                        </div>
+
+                        <div class="mb-5">
+                            We are seeking a talented Front-End Developer to join our team in Boston, MA. The ideal
+                            candidate will have strong skills in HTML, CSS, and JavaScript...
+                        </div>
+
+                        <h3 class="text-green-500 mb-2">$70 - $80K / Year</h3>
+
+                        <div class="border border-gray-100 mb-5"></div>
+
+                        <div class="flex flex-col lg:flex-row justify-between mb-4">
+                            <div class="text-orange-700 mb-3">
+                                <i class="fa-solid fa-location-dot text-lg"></i>
+                                Boston, MA
+                            </div>
+                            <a
+                                href="job.html"
+                                class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+                            >
+                                Read More
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <!-- Job Listing 2 -->
+                <div class="bg-white rounded-xl shadow-md relative">
+                    <div class="p-4">
+                        <div class="mb-6">
+                            <div class="text-gray-600 my-2">Remote</div>
+                            <h3 class="text-xl font-bold">Front-End Engineer (Vue)</h3>
+                        </div>
+
+                        <div class="mb-5">
+                            Join our team as a Front-End Developer in sunny Miami, FL. We are looking for a motivated
+                            individual with a passion...
+                        </div>
+
+                        <h3 class="text-green-500 mb-2">$70K - $80K / Year</h3>
+
+                        <div class="border border-gray-100 mb-5"></div>
+
+                        <div class="flex flex-col lg:flex-row justify-between mb-4">
+                            <div class="text-orange-700 mb-3">
+                                <i class="fa-solid fa-location-dot text-lg"></i>
+                                Miami, FL
+                            </div>
+                            <a
+                                href="job.html"
+                                class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+                            >
+                                Read More
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <!-- Job Listing 3 -->
+                <div class="bg-white rounded-xl shadow-md relative">
+                    <div class="p-4">
+                        <div class="mb-6">
+                            <div class="text-gray-600 my-2">Remote</div>
+                            <h3 class="text-xl font-bold">Vue.js Developer</h3>
+                        </div>
+
+                        <div class="mb-5">
+                            Are you passionate about front-end development? Join our team in vibrant Brooklyn, NY, and
+                            work on exciting projects that make a difference...
+                        </div>
+
+                        <h3 class="text-green-500 mb-2">$70K - $80K / Year</h3>
+
+                        <div class="border border-gray-100 mb-5"></div>
+
+                        <div class="flex flex-col lg:flex-row justify-between mb-4">
+                            <div class="text-orange-700 mb-3">
+                                <i class="fa-solid fa-location-dot text-lg"></i>
+                                Brooklyn, NY
+                            </div>
+                            <a
+                                href="job.html"
+                                class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+                            >
+                                Read More
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
